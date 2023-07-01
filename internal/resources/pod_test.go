b0b540b78ec853ed3cb596520066f8533eddec7a
Markus Ongyerth <ongy@ongy.net>
2023-07-01 22:09:26 +0200

    Improve registry testing tooling

--- internal/resources/pod_test.go
+++ internal/resources/pod_test.go
@@ -2,6 +2,8 @@ package resources
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -89,7 +91,58 @@ var (
 	configFile = []byte(`{"architecture":"amd64","config":{"Env":["PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin","PYTHONUNBUFFERED=1"],"Entrypoint":["/usr/bin/radicale"],"WorkingDir":"/","OnBuild":null},"created":"2023-04-19T09:03:58.935540517+02:00","history":[{"created":"2023-02-11T04:46:42.449083344Z","created_by":"/bin/sh -c #(nop) ADD file:40887ab7c06977737e63c215c9bd297c0c74de8d12d16ebdf1c3d40ac392f62d in / "},{"created":"2023-02-11T04:46:42.558343068Z","created_by":"/bin/sh -c #(nop)  CMD [\"/bin/sh\"]","empty_layer":true},{"created":"2023-04-19T09:03:51.066438359+02:00","created_by":"ENV PYTHONUNBUFFERED=1","comment":"buildkit.dockerfile.v0","empty_layer":true},{"created":"2023-04-19T09:03:51.066438359+02:00","created_by":"RUN /bin/sh -c apk add --update --no-cache python3 \u0026\u0026 ln -sf python3 /usr/bin/python # buildkit","comment":"buildkit.dockerfile.v0"},{"created":"2023-04-19T09:03:58.935540517+02:00","created_by":"RUN /bin/sh -c python3 -m ensurepip \u0026\u0026 pip3 install --no-cache --upgrade radicale # buildkit","comment":"buildkit.dockerfile.v0"},{"created":"2023-04-19T09:03:58.935540517+02:00","created_by":"WORKDIR /","comment":"buildkit.dockerfile.v0","empty_layer":true},{"created":"2023-04-19T09:03:58.935540517+02:00","created_by":"ENTRYPOINT [\"/usr/bin/radicale\"]","comment":"buildkit.dockerfile.v0","empty_layer":true}],"moby.buildkit.buildinfo.v1":"eyJmcm9udGVuZCI6ImRvY2tlcmZpbGUudjAiLCJhdHRycyI6eyJmaWxlbmFtZSI6IkRvY2tlcmZpbGUucmFkaWNhbGUifSwic291cmNlcyI6W3sidHlwZSI6ImRvY2tlci1pbWFnZSIsInJlZiI6ImRvY2tlci5pby9saWJyYXJ5L2FscGluZTpsYXRlc3QifSx7InR5cGUiOiJkb2NrZXItaW1hZ2UiLCJyZWYiOiJkb2NrZXIuaW8vbGlicmFyeS9hbHBpbmU6bGF0ZXN0IiwicGluIjoic2hhMjU2OjdjZDUyODQ3YWQ3NzVhNWRkYzRiNTgzMjZjZjg4NGJlZWUzNDU0NDI5NjQwMmM2MjkyZWQ3NjQ3NGM2ODZkMzkifV19","os":"linux","rootfs":{"type":"layers","diff_ids":["sha256:7cd52847ad775a5ddc4b58326cf884beee34544296402c6292ed76474c686d39","sha256:fd389f3de3f9ba4f223e98c46e8c235fd5dd1b304d604da756569e15ae714a78","sha256:049c3bf18ff4305edb25e5c6f4a13b51525de623b7a0809d2a930d2c126c78e5"]}}`)
 )
 
+type fileInfo struct {
+	path        string
+	content     []byte
+	contentType string
+}
+
+func makeManifestFile(path string, config fileInfo) fileInfo {
+	manifest := registryv1.Manifest{
+		SchemaVersion: 2,
+		MediaType:     "application/vnd.docker.distribution.manifest.v2+json",
+		Config: registryv1.Descriptor{
+			MediaType: "application/vnd.docker.container.image.v1+json",
+			Size:      int64(len(config.content)),
+			Digest:    registryv1.Hash{Algorithm: "sha256", Hex: config.path},
+		},
+	}
+
+	content, _ := json.Marshal(manifest)
+
+	return fileInfo{
+		path:        path,
+		content:     content,
+		contentType: "application/vnd.docker.distribution.manifest.v2+json",
+	}
+}
+
+func makeArchConfig(architecture string) fileInfo {
+	content := []byte(fmt.Sprintf(`{"architecture":%q}`, architecture))
+
+	hasher := sha256.New()
+	hasher.Write(content)
+
+	hash := hasher.Sum([]byte{})
+	dst := make([]byte, len(hash)*2)
+	hex.Encode(dst, hash)
+
+	return fileInfo{
+		path:        string(dst),
+		content:     content,
+		contentType: "application/vnd.docker.container.image.v1+json",
+	}
+}
+
+func makeSingleInfos(architecture, org, image string) []fileInfo {
+	config := makeArchConfig(architecture)
+	manifest := makeManifestFile(fmt.Sprintf("/v2/%s/%s/manifests/latest", org, image), config)
+
+	return []fileInfo{config, manifest}
+}
+
 type testTripper struct {
+	files []fileInfo
 }
 
 func (t *testTripper) RoundTrip(req *http.Request) (*http.Response, error) {
@@ -99,50 +152,25 @@ func (t *testTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 			Body:       io.NopCloser(bytes.NewBufferString("{}")),
 		}, nil
 	}
-	if req.URL.String() == "http://registry.local/v2/org/image/manifests/latest" {
-		mediaType := "application/vnd.docker.distribution.manifest.v2+json"
-		manifest := registryv1.Manifest{
-			SchemaVersion: 2,
-			MediaType:     "application/vnd.docker.distribution.manifest.v2+json",
-			Config: registryv1.Descriptor{
-				MediaType: "application/vnd.docker.container.image.v1+json",
-				Size:      2062,
-				Digest:    registryv1.Hash{Algorithm: "sha256", Hex: "5418ac5908700db2ef517c0e9d167f4c1309d97084d33dd5635c01272297ebeb"},
-			},
-			Layers: []registryv1.Descriptor{
-				{
-					MediaType: "application/vnd.docker.image.rootfs.diff.tar.gzip",
-					Size:      3374446,
-					Digest:    registryv1.Hash{Algorithm: "sha256", Hex: "63b65145d645c1250c391b2d16ebe53b3747c295ca8ba2fcb6b0cf064a4dc21c"},
-				},
-			},
+
+	for _, file := range t.files {
+		if req.URL.Path == file.path || strings.HasSuffix(req.URL.Path, fmt.Sprintf("/sha256:%s", file.path)) {
+			if accept, ok := req.Header["Accept"]; ok {
+				if !slices.Contains(strings.Split(accept[0], ","), file.contentType) {
+					continue
+				}
+			}
+
+			return &http.Response{
+				StatusCode:    http.StatusOK,
+				Body:          io.NopCloser(bytes.NewBuffer(file.content)),
+				ContentLength: int64(len(file.content)),
+				Header: http.Header{
+					"Content-Type": []string{file.contentType},
+				}}, nil
 		}
-		body, err := json.Marshal(manifest)
-		if err != nil {
-			return nil, fmt.Errorf("Failed to marshal manifest: %v", err)
-		}
-		if !slices.Contains(strings.Split(req.Header["Accept"][0], ","), mediaType) {
-			fmt.Printf("Fixing up answer!\n")
-			body = firstManifest
-			mediaType = "application/vnd.docker.distribution.manifest.v1+json"
-		}
-		return &http.Response{
-			StatusCode: http.StatusOK,
-			Body:       io.NopCloser(bytes.NewBuffer(body)),
-			Header: http.Header{
-				"Content-Type": []string{mediaType},
-			},
-		}, nil
-	}
-	if req.URL.String() == "http://registry.local/v2/org/image/blobs/sha256:5418ac5908700db2ef517c0e9d167f4c1309d97084d33dd5635c01272297ebeb" {
-		return &http.Response{
-			StatusCode: http.StatusOK,
-			Body:       io.NopCloser(bytes.NewBuffer(configFile)),
-			ContentLength: 2062,
-		}, nil
 	}
 
-	fmt.Printf("Roundtripping URL: %v, %v\n", req.URL, req.Header)
 	return nil, errors.New("Not implemented yet")
 }
 
@@ -162,7 +190,7 @@ func TestContainerArchitectures(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			registry.DefaultTransport = &testTripper{}
+			registry.DefaultTransport = &testTripper{makeSingleInfos(testCase.expected[0], "org", "image")}
 			arches, err := containerArchitectures(testCase.input)
 			if err != nil {
 				t.Fatalf("Failed to get container architectures: %v", err)
